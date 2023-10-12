37413f812d89068ea478a91a14f2172576978863
AdielFranstito <127399830+AdielFranstito@users.noreply.github.com>
2023-10-12 19:19:37 +0700

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,13 @@
+function jumlahkan() {
+  var bilangan1 = parseFloat(document.getElementById("bilangan1").value);
+  var bilangan2 = parseFloat(document.getElementById("bilangan2").value);
+  var hasil = bilangan1 + bilangan2;
+  document.getElementById("hasil").innerHTML = "Hasil penjumlahan: " + hasil;
+  alert("Hasil:" + hasil);
+}
+
+function ulang() {
+  document.getElementById("bilangan1").value = "";
+  document.getElementById("bilangan2").value = "";
+  document.getElementById("hasil").innerHTML = "";
+}
